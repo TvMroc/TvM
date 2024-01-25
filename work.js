f988031d079dddfb255595c6c4446170c67cba34
TvMroc <1207837@student.roc-nijmegen.nl>
2024-01-25 23:17:18 +0100

    contact page

--- work.js
+++ work.js
@@ -3,7 +3,7 @@ function scrollto(direction) {
 }
 
 function redirect(site, distance) {
-    document.getElementById(`${site}`).style.height = "100"+"vh";
+    document.getElementById(site).style.height = "100"+"vh";
     document.getElementById('imgScroll').style.marginTop = "-202"+"px";
     document.getElementById('imgScroll').style.marginLeft = "-2"+"px";
     document.getElementById('imgScroll').style.width = "100"+"vw";
@@ -12,6 +12,14 @@ function redirect(site, distance) {
     document.getElementById('scrollBar').scrollTo(distance, 0);
     window.scrollTo(0, 0);
     setTimeout(function(){
-        window.location.replace(`${site}/index.html`);
+        window.open(`${site}/index.html`, "_blank");
     }, 500);
+    setTimeout(function(){
+        document.getElementById(site).style.height = "600"+"px";
+        document.getElementById('imgScroll').style.marginTop = "120"+"px";
+        document.getElementById('imgScroll').style.marginLeft = "0"+"px";
+        document.getElementById('imgScroll').style.width = "100"+"%";
+        document.getElementById('scrollBar').style.width = "90"+"vw";
+        document.getElementById('scrollBar').style.overflowX = "auto";
+    }, 2000);
 }
